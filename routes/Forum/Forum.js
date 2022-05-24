3142e9e93bb365fc5725c3b3f1b3f15885083f61
Penny <583forgame@gmail.com>
2022-05-24 21:20:28 +0800

    增新文章，需調整資料庫否則無法完整實現

--- routes/Forum/Forum.js
+++ routes/Forum/Forum.js
@@ -2,51 +2,26 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../modules/mysql_config");
 const multer = require("multer");
+const app = require("../../app");
 
+// 文章依分類顯示
 router.route("/")
   .get(async (req, res, next) => {
     if (req.query.topic == "null") {
-      console.log('null!!!');
-      const sql = "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn JOIN`users` ON blog_article.users_id = users.id";
+      //console.log('is null');
+      const sql = "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn JOIN`users` ON blog_article.users_id = users.id ORDER BY `blog_article`.`created_time` DESC";
       const [datas] = await db.query(sql);
       res.json(datas);
-
     } else {
-      console.log(req.query);
+      //console.log(req.query);
       const topic = decodeURI(`${req.query.topic}`);
       const sql =
-        "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn HAVING thema = ?";
+        "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn HAVING thema = ? ORDER BY `blog_article`.`created_time` DESC";
       const [datas] = await db.query(sql, [topic]);
       res.json(datas);
     }
   })
-// router.route("/")
-//   .get(async (req, res, next) => {
-//     if (req.query !== null) {
-//       console.log(req.query);
-//       const topic = decodeURI(`${req.query.topic}`);
-//       const sql =
-//         "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn HAVING thema = ?";
-//       const [datas] = await db.query(sql, [topic]);
-//       res.json(datas);
-//     } else {
-//       console.log('null!!!');
-//       const sql = "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn JOIN`users` ON blog_article.users_id = users.id";
-//       const [datas] = await db.query(sql);
-//       res.json(datas);
-
-
-//     }
-//   })
-
-// router.route("/")
-//   .get(async (req, res, next) => {
-
-//     const sql = "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn JOIN`users` ON blog_article.users_id = users.id";
-//     const [datas] = await db.query(sql);
-//     res.json(datas);
-//   })
-
+//分類
 router.route("/category")
   .get(async (req, res, next) => {
     const sql =
@@ -55,25 +30,25 @@ router.route("/category")
     res.json(datas);
   })
 
-
-// router.route("/addarticle").post((req, res, next) => {
-//   // const sql =
-//   //   "INSERT INTO blog_article(title,content,created_time,category) VALUES (?,?,{Date.now()},?)";
-//   // // const [datas] = await db.query(sql,[req.body]);
-//   // const [datas] = await db.query(sql, [
-//   //   req.body.title,
-//   //   req.body.content,
-//   //   req.body.created_time,
-//   //   red.body.category,
-//   // ]);
-//   // console.log(datas);
-//   res.send("新增資料");
-// });
-
-
-router.route("/Btn").get(async (req, res, next) => {
-  res.json(["好展推薦", "小資攻略", "閒聊交流", "心得分享"]);
+router.route("/addarticle").post(async (req, res, next) => {
+  // if (req.body.title === "iii" && req.body.content === "555") {
+  //   console.log("sugoi");
+  // } else {
+  //   console.log(req.body);
+  // }
+  if (req.body) {
+    const sql =
+      // "INSERT INTO `blog_article`(`title`, `content`, `created_time`, `category`, `users_id`) VALUES (?,?,'20220505',?,?);";
+      "INSERT INTO `blog_article`(`title`, `content`, `created_time`, `category`, `users_id`) VALUES (?,?,NOW(),? ,2 );";
+    const [datas] = await db.query(sql, [
+      req.body.title,
+      req.body.content,
+      req.body.category,
+    ]);
+    res.send(datas);
+  }
 });
+
 
 //個別文章
 router.route("/:id").get(async (req, res, next) => {
@@ -85,14 +60,5 @@ router.route("/:id").get(async (req, res, next) => {
   res.json(datas[0][0]);
 });
 
-//分類篩選
-// router.route(`/?topic=:topic`).get((req, res, next) => {
-//   const topic = decodeURI(req.params.topic);
-//   const sql =
-//     "SELECT * FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn WHERE thema=?";
-//   const datas = db.query(sql, [topic]);
-//   console.log(topic);
-//   res.json(datas[0]);
-// });
 
 module.exports = router;
