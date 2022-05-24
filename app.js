a1c5cdb847fd7bb2ff08897e2d3ac7310110daa5
Penny <583forgame@gmail.com>
2022-05-24 21:21:01 +0800

    移除tinyMCE圖片上傳路徑

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const bookingRouter = require('./routes/Booking/booking');
 const ForumRouter = require('./routes/Forum/Forum');
 const productRouter = require('./routes/Product/product');
 const B2BRouter = require('./routes/B2B/B2B');
-const uploadIMGRouter = require('./routes/Forum/TinyMCEuploadImg');
 
 
 
@@ -49,9 +48,6 @@ app.use('/users', usersRouter);
 app.use('/product', productRouter);
 app.use('/forum', ForumRouter);
 app.use('/B2B', B2BRouter);
-
-//TinyMCE圖片上傳API
-app.use('/uploadIMG', uploadIMGRouter);
 
 
 
