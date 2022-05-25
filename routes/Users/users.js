0357169401234f1e13e52c5e2de6ad81d354a6a4
Penny <583forgame@gmail.com>
2022-05-25 17:54:45 +0800

    會員個人主頁api設定

--- routes/Users/users.js
+++ routes/Users/users.js
@@ -10,4 +10,16 @@ router.get('/', function (req, res, next) {
 
 
 
+
+// 個人主頁文章顯示-佩珊
+router.route("/personalpage")
+  .post(async (req, res, next) => {
+    const id = req.body.userID;
+    //console.log(`目前登入使用者id=${id}`);
+    const sql = "SELECT article_id, title, created_time, content, users_id, thema, nickname, username FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn JOIN`users` ON blog_article.users_id = users.id WHERE users_id = ? ORDER BY `blog_article`.`created_time` DESC";
+    const datas = await db.query(sql, [id]);
+    res.json(datas[0]);
+  })
+
+
 module.exports = router;
