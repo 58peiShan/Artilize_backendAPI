44a37ddb03be69a202cb32c281792b9729a060c1
JosephYu0326 <pkujm951@gmail.com>
2022-06-02 17:38:10 +0800

    update users login

--- routes/Users/users.js
+++ routes/Users/users.js
@@ -6,8 +6,14 @@ const multer = require('multer');
 const upload = multer();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('User');
+router.get('/', async (req, res, next)=> {
+  
+  console.log(req.query.userId)
+  const{userId} = req.query
+  const sql = 'SELECT userAccount,userAddress,userAvatar,userBirthday,userEmail,userGender,userMobile,userName,userNickName FROM users WHERE userId=?'
+  const [datas] = await db.query(sql,[userId])
+  console.log(datas)
+  res.send(datas)
 });
 
 router.post('/signup',upload.none(), async function (req, res, next) {
@@ -17,7 +23,7 @@ router.post('/signup',upload.none(), async function (req, res, next) {
   let data = JSON.parse(JSON.stringify(req.body))
   const{userAccount,userEmail,userPassword} = data
   const sql = "INSERT INTO users(userAccount,userPassword,userEmail) VALUES(?,?,?)"
-  const [datas] = await db.query(sql,[userAccount,userEmail,userPassword])
+  const [datas] = await db.query(sql,[userAccount,userPassword,userEmail])
   console.log(datas)
   if(datas.affectedRows === 1){
     ouput.ok = true
@@ -26,6 +32,7 @@ router.post('/signup',upload.none(), async function (req, res, next) {
  
 });
 
+//Account 驗證
 router.get('/signup/checkaccount',async function(req,res,next){
   let output = {
       canUse: false
@@ -44,6 +51,51 @@ router.get('/signup/checkaccount',async function(req,res,next){
     res.json(output)
   }
 })
+
+//Email 驗證
+router.get('/signup/checkemail', async (req,res,next)=>{
+    let output ={
+      canUse: false
+    }
+    const sql = 'SELECT Count(*) as total FROM users WHERE userEmail=?'
+    const [datas]= await db.query(sql,[req.query.email])
+    const{total}=datas[0]
+    console.log(total)
+    if(total>0){
+      res.json(output)
+    }else{
+      output.canUse = true
+      res.json(output)
+    }
+
+})
+
+//登入
+router.post('/login',upload.none(),async (req,res,next)=>{
+  let output={
+    ok:false
+  }
+  const info =JSON.parse(JSON.stringify(req.body))
+  //console.log(info)
+  const {userAccount,userPassword}=info
+  const sql = 'SELECT userId,userAccount,userPassword FROM users WHERE userAccount=? and userPassword=?'
+  const [datas] =  await db.query(sql,[userAccount,userPassword])
+  console.log(datas)
+  
+  
+  if(datas.length>0){
+     output.ok = true
+     output.userId = datas[0].userId
+
+     console.log(output)
+     res.json(output)
+  }else{
+    console.log(output)
+    res.json(output)
+  }
+
+})
+
 
 
 
