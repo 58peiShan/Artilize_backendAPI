68bbd24de13f28e08c36e78c91543a77df777660
penny <583forgame@gmail.com>
2022-06-07 14:51:01 +0800

    個人頁面時間格式修正

--- routes/Users/users.js
+++ routes/Users/users.js
@@ -16,7 +16,7 @@ router.route("/personalpage")
   .post(async (req, res, next) => {
     const id = req.body.userID;
     //console.log(`目前登入使用者id=${id}`);
-    const sql = "SELECT article_id, title, created_time, content, users_id, thema, nickname, username FROM blog_article JOIN `blog_category` ON blog_article.category = blog_category.sn JOIN`users` ON blog_article.users_id = users.id WHERE users_id = ? ORDER BY `blog_article`.`created_time` DESC";
+    const sql = `SELECT article_id, title, DATE_FORMAT(created_time, "%Y-%m-%d") AS created_time, content, users_id, thema, nickname, username FROM blog_article JOIN blog_category ON blog_article.category = blog_category.sn JOIN users ON blog_article.users_id = users.id WHERE users_id = ? ORDER BY blog_article.created_time DESC`;
     const datas = await db.query(sql, [id]);
     res.json(datas[0]);
   })
