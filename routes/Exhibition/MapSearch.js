87b8e870767fffb701ef780db5660a454182201d
JosephYu0326 <pkujm951@gmail.com>
2022-06-08 00:25:51 +0800

    地圖 修改

--- routes/Exhibition/MapSearch.js
+++ routes/Exhibition/MapSearch.js
@@ -3,17 +3,17 @@ var router = express.Router();
 const db = require('../../modules/mysql_config');
 
 router.get('/', async function (req, res, next) {
-    const sql = `SELECT * from exhibition`
+    const sql = `SELECT exhibition_actadm.id,aName,pic1,date_format(start,"%Y-%m-%d") as start ,date_format(end,"%Y-%m-%d") as end,direction,city,mName,longitude,latitude from exhibition_actadm left JOIN exhibition_city on cid = fkCityId JOIN exhibition_museum ON mid = fkMuseumId JOIN exhibition_kind on kid = fkKindId JOIN exhibition_direction on exhibition_direction.id = exhibition_city.direction_id`
     const [datas] = await db.query(sql);
     res.json(datas);
     console.log(datas)
 });
-router.get('/ability', async function (req, res, next) {
-    const sql = `SELECT * from ability`
-    const [datas] = await db.query(sql);
-    res.json(datas);
-    console.log(datas)
-});
+// router.get('/ability', async function (req, res, next) {
+//     const sql = `SELECT * from ability`
+//     const [datas] = await db.query(sql);
+//     res.json(datas);
+//     console.log(datas)
+// });
 
 
 
